77c408294e8443c34ffa93130af11c8ff4074efb
Tom Reed <t.reed1104@gmail.com>
2020-11-19 23:23:35 +0000

    Amended my curly-brace setting to be new-line for readability

--- Program.cs
+++ Program.cs
@@ -1,23 +1,29 @@
 ﻿using System;
 
-namespace change_calculator {
-    class Program {
-        private static string RemovePoundSymbol(string inputString) {
+namespace change_calculator
+{
+    class Program
+    {
+        private static string RemovePoundSymbol(string inputString)
+        {
             // Check if the string contains a £ - catchs inputs with pre-post £ symbols, inputs such as '3£4' will be marked invalid as theyre unclear
-            if (inputString.Contains('£')) {
+            if (inputString.Contains('£'))
+            {
                 inputString = inputString.Replace('£', ' ');    // Replace the £ with a space
                 inputString = inputString.Trim();               // Trim the pre-post spaces
             }
             return inputString;
         }
 
-        public static double GetUserInput(string messageToPrint) {
+        public static double GetUserInput(string messageToPrint)
+        {
             // Get the user input and sanitise that its a valid numerical value we can use
             Console.WriteLine(messageToPrint);   // Print the message to console to instruct the user
             string inputString = RemovePoundSymbol(Console.ReadLine()); // Get input and remove £ symbols
             double inputConverted = 0;
             // Check the input can be converted to a double
-            while (!double.TryParse(inputString, out inputConverted)) {
+            while (!double.TryParse(inputString, out inputConverted))
+            {
                 // Invalid input, inform the user and try again
                 Console.WriteLine("Invalid value input, please make sure you are entering a number");
                 inputString = RemovePoundSymbol(Console.ReadLine());
@@ -25,16 +31,24 @@ namespace change_calculator {
             return inputConverted;
         }
 
-        private static void OutputChangeDistribution(double[] validChangeValues, int[] changeDistribution) {
+        private static void OutputChangeDistribution(double[] validChangeValues, int[] changeDistribution)
+        {
             // Output the change distribution to the console
             string changeOutputText = "The change to return is:";
 
             Console.WriteLine(changeOutputText);
         }
 
-        public static void CalculateChange(double[] validChangeValues, double inputCost, double inputPayment) {
+        public static void CalculateChange(double[] validChangeValues, double inputCost, double inputPayment)
+        {
             // Calculate the distribution of valid change types
-            if (inputCost == inputPayment) { Console.WriteLine("You paid with exact change!"); return; }    // Check if the user enterred exact change
+
+            // Check if the user enterred exact change
+            if (inputCost == inputPayment)
+            {
+                Console.WriteLine("You paid with exact change!");
+                return;
+            }    
 
             double changeToReturn = inputPayment - inputCost;       // Calculate the change delta
             int remainingChange = (int)(changeToReturn * 100);      // Our running total of change in pence -> gets round decimal issues e.g. when we sometimes get 0.9999999999998 instead of 1 with doubles
@@ -43,10 +57,19 @@ namespace change_calculator {
             int[] changeDistribution = new int[validChangeValues.Length];     // Maps directly to the input change value array - e.g. changeValue[0] which is £50 notes directly matches changeDistribution[0] for its count
 
             // For each valid change type, check how it goes into the remaining value
-            for (int i = 0; i < validChangeValues.Length; i++) {
-                if (remainingChange == 0) { break; }                                    // We've run out of change to split
+            for (int i = 0; i < validChangeValues.Length; i++)
+            {
+                // We've run out of change to split
+                if (remainingChange == 0)
+                {
+                    break;
+                }
                 int changeValueInPence = (int)(validChangeValues[i] * 100);             // Convert the change type to its value in pence
-                if (changeValueInPence > (int)(changeToReturn * 100)) { continue; }     // Check we aren't using a change type higher than the change we are distributing
+                // Check we aren't using a change type higher than the change we are distributing
+                if (changeValueInPence > (int)(changeToReturn * 100))
+                {
+                    continue;
+                }
 
                 // Calculate how the change is to be distributed
                 int currentChangeToSplit = remainingChange - (remainingChange % changeValueInPence);    // Calculate the value to split with the current change type
@@ -58,28 +81,34 @@ namespace change_calculator {
             OutputChangeDistribution(validChangeValues, changeDistribution);
         }
 
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
             bool hasUserProvidedValidInputs = false;
             double[] validChangeTypes = new double[] { 50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01 };
 
-            while (!hasUserProvidedValidInputs) {
+            while (!hasUserProvidedValidInputs)
+            {
                 // Get user inputs
                 double productCost = GetUserInput("Enter the product cost (E.g. £4.50):");
-                if (productCost <= 0) {
+                if (productCost <= 0)
+                {
                     Console.WriteLine("A purchase must cost more than £0");
                     continue;
                 }
                 double userPayment = GetUserInput("Enter the value you are paying with (£20):");
-                if (userPayment <= 0) {
+                if (userPayment <= 0)
+                {
                     Console.WriteLine("The payment value must be more than £0");
                     continue;
                 }
                 // Check the user is paying enough for the product
-                if (userPayment >= productCost) {
+                if (userPayment >= productCost)
+                {
                     hasUserProvidedValidInputs = true;
                     CalculateChange(validChangeTypes, productCost, userPayment);
                 }
-                else {
+                else
+                {
                     Console.WriteLine("The payment value is less than the cost, more money is required for the purchase");
                 }
             }
