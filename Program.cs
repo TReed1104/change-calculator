b7e61d6f0f19c0020de02773a83e88d48fb1c43a
Tom Reed <t.reed1104@gmail.com>
2020-11-19 22:43:28 +0000

    Wrapped the user input readlines with the RemovePoundSymbol call

--- Program.cs
+++ Program.cs
@@ -14,24 +14,24 @@ namespace change_calculator {
         static void Main(string[] args) {
             // Get input for product price
             Console.WriteLine("Enter the product cost");
-            string productPrice = Console.ReadLine();
+            string productPrice = RemovePoundSymbol(Console.ReadLine());
             double priceConverted = 0;
             // Check the input can be converted to a double
             while (!double.TryParse(productPrice, out priceConverted)) {
                 // Invalid input, inform the user and try again
                 Console.WriteLine("Invalid value input");
-                productPrice = Console.ReadLine();
+                productPrice = RemovePoundSymbol(Console.ReadLine());
             }
 
             // Get input for product price
             Console.WriteLine("Enter the value you are paying");
-            string paymentValue = Console.ReadLine();
+            string paymentValue = RemovePoundSymbol(Console.ReadLine());
             double paymentConverted = 0;
             // Check the input can be converted to a double
             while (!double.TryParse(paymentValue, out paymentConverted)) {
                 // Invalid input, inform the user and try again
                 Console.WriteLine("Invalid value input");
-                paymentValue = Console.ReadLine();
+                paymentValue = RemovePoundSymbol(Console.ReadLine());
             }
         }
     }
