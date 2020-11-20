efbbbe1287273f4b6f06b73d1945e3809801e12d
Tom Reed <t.reed1104@gmail.com>
2020-11-20 11:00:11 +0000

    Tests for ConvertToPence

--- change-calculator.tests/ChangeCalculatorTests.cs
+++ change-calculator.tests/ChangeCalculatorTests.cs
@@ -6,6 +6,34 @@ namespace change_calculator.tests
 {
     public class ChangeCalculatorTests
     {
+        [Theory]
+        [InlineData(1, 100)]
+        [InlineData(10, 1000)]
+        [InlineData(55, 5500)]
+        public void Test_ConvertToPence_CorrectValue(double input, int expected)
+        {
+            // Correct expected result
+            Assert.Equal(expected, Program.ConvertToPence(input));
+        }
+
+        [Theory]
+        [InlineData(2, 100)]
+        [InlineData(200, 100)]
+        [InlineData(232, 55500)]
+        public void Test_ConvertToPence_IncorrectValue(double input, int expected)
+        {
+            // Incorrect expected result
+            Assert.NotEqual(expected, Program.ConvertToPence(input));
+        }
+
+        [Theory]
+        [InlineData(5.5, 550)]
+        [InlineData(3.3, 330)]
+        [InlineData(3.42, 342)]
+        public void Test_ConvertToPence_DecimalValue(double input, int expected)
+        {
+            Assert.Equal(expected, Program.ConvertToPence(input));
+        }
     }
 
 }
